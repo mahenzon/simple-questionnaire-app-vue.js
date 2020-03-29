419e1b45740681348d6f48dd369ed2650a3773d6
Suren Khorenyan <surenkhorenyan@gmail.com>
2020-03-30 00:04:54 +0300

    Create Survey component, remove HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Survey msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Survey from './components/Survey'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Survey,
   },
 }
 </script>
