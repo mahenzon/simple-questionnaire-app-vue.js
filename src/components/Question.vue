76a6becfd0326b3316e65875bb28fb3fa5197e1e
Suren Khorenyan <surenkhorenyan@gmail.com>
2020-03-30 02:37:46 +0300

    Create QuestionSelect component

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -12,18 +12,27 @@
     :question="question"
     />
 
+    <QuestionSelect
+    v-if="question.type === 'select'"
+    v-on:sendAnswer="sendAnswer"
+    :question="question"
+    />
+
+
   </div>
 </template>
 
 <script>
 import QuestionText from "./question-types/QuestionText"
 import QuestionEmail from "./question-types/QuestionEmail"
+import QuestionSelect from "./question-types/QuestionSelect"
 
 export default {
   name: "Question",
   components: {
     QuestionText,
     QuestionEmail,
+    QuestionSelect,
   },
   props: {
     question: Object,
---
+++ src/components/question-types/QuestionSelect.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-card :title="question.text" :sub-title="question.subtitle">
+    <b-form-select v-model="selected" :options="question.options"></b-form-select>
+    <ButtonNext
+    class="mt-2"
+    v-on:goNext="sendAnswer"
+    :disabled="!selected"
+    :variant="selected && 'primary' || 'dark'"
+    />
+  </b-card>
+</template>
+
+<script>
+import ButtonNext from "./sub-types/ButtonNext"
+
+export default {
+  name: "QuestionSelect",
+  components: {
+    ButtonNext,
+  },
+  props: {
+    question: Object,
+  },
+  data() {
+    return {
+      selected: null,
+    }
+  },
+  methods: {
+    sendAnswer() {
+      this.$emit("sendAnswer", {result: this.selected})
+      this.selected = null
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
